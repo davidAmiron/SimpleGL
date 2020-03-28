821faf2c51ff586c059afc4bbdfcc80f2cc946cd
David Miron <david.miron@duke.edu>
2020-03-28 12:49:14 -0400

    Change interfaces, start implementing drawing

--- include/simplegl.h
+++ include/simplegl.h
@@ -1,35 +1,61 @@
-
+#include <string>
+#include <vector>
+#include "SDL2/SDL.h"
 
 class SimpleGL {
 
 public:
 
     /**
+     * Represent a color. Values are between 0 and 1.
+     */
+    typedef struct Color {
+        float red;
+        float green;
+        float blue;
+        float alpha;
+    } Color_t;
+
+    /**
      * Create a SimpleGL object
      *
+     * @param title The title of the window
+     * @param loc_x The x location of the window
+     * @param loc_y The y location of the window
      * @param height The window height in pixels
      * @param width The window width in pixels
      */
-    SimpleGL(int width, int height);
+    SimpleGL(std::string title, int loc_x, int loc_y, int width, int height);
 
     /**
      * Initialize the window
      */
-    void init();
+    void Init();
 
     /**
      * Update the window
+     *
+     * @returns true if still running, false if window has been quit
      */
-    void update();
+    bool Update();
+
+    /**
+     * Draw a triangle
+     * @param x1, y1 The location of the first point
+     * @param x2, y2 The location of the second point
+     * @param x3, y3 The location of the third point
+     * @param color The color of the triangle
+     */
+    void DrawTriangle(int x1, int y1, int x2, int y2, int x3, int y3, SimpleGL::Color color);
 
     /**
      * Draw a rectangle
      *
-     * @param x, y The location of the top right corner of the rectangle
+     * @param x, y The location of the top left corner of the rectangle
      * @param width The width of the rectangle
      * @param height The height of the rectangle
      */
-    void drawRect(int x, int y, int width, int height);
+    void DrawRect(int x, int y, int width, int height, SimpleGL::Color color);
 
     /**
      * Draw a line
@@ -37,11 +63,61 @@ public:
      * @param x1, y1 Location of first point of line
      * @param x2, y2 Location of second point of line
      */
-    void drawLine(int x1, int y1, int x2, int y2);
+    void DrawLine(int x1, int y1, int x2, int y2, SimpleGL::Color color);
+
+    /**
+     * Destroy the object, and clean up
+     */
+    void Destroy();
 
 private:
 
-    int windowWidth_, windowHeight_;
+    void Draw();
 
+    std::string window_title_;
+    int window_loc_x_, window_loc_y_, window_width_, window_height_;
+
+    SDL_Window *window_;
+    SDL_GLContext gl_context_;
+
+    GLuint vertex_shader_;
+    GLuint fragment_shader_;
+
+    std::vector<float> triangle_vertices_;
+    std::vector<float> line_vertices_;
+    GLuint triangles_vbo_;
+    GLuint lines_vbo_;
+    std::vector<float> triangle_elements_;
+    std::vector<float> line_elements_;
+    GLuint triangles_ebo_;
+    GLuint lines_ebo_;
+
+    const char* kVertexShaderSource = R"glsl(
+        #version 150 core
+
+        in vec2 position;
+        in vec3 color;
+
+        out vec3 color_out;
+    
+        void main()
+        {
+            gl_Position = vec4(position, 0.0, 1.0);
+            color_out = color;
+        }
+    )glsl";
+
+    const char* kFragmentShaderSource = R"glsl(
+        #version 150 core
+
+        in vec3 color;
+        
+        out vec4 color_out;
+
+        void main()
+        {
+            color_out = color;
+        }
+    )glsl";
 
 };
--- src/simplegl.cc
+++ src/simplegl.cc
@@ -1,24 +1,131 @@
-#include <iostream>
+#define GLEW_STATIC
+
+#include "GL/glew.h"
+#include "SDL2/SDL.h"
+
 #include "simplegl.h"
 
-SimpleGL::SimpleGL(int width, int height) :
-        windowWidth_(width), windowHeight_(height)
+#include <vector>
+#include <iostream>
+
+SimpleGL::SimpleGL(std::string title, int loc_x, int loc_y, int width, int height) :
+        window_title_(title), window_loc_x_(loc_x), window_loc_y_(loc_y),
+        window_width_(width), window_height_(height)
 {
     std::cout << "Created" << std::endl;
 }
 
-void SimpleGL::init()
+void SimpleGL::Init()
+{
+    // Initialize SDL2
+    SDL_Init(SDL_INIT_VIDEO);
+
+    // Initialize attributes for OpenGL
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+
+    // Create window and OpenGL context
+    window_ = SDL_CreateWindow(window_title_.c_str(), window_loc_x_, window_loc_y_,
+                    window_width_, window_height_, SDL_WINDOW_OPENGL);
+    gl_context_ = SDL_GL_CreateContext(window_);
+
+    // Initialize GLEW
+    glewExperimental = GL_TRUE;
+    glewInit();
+
+    // Use Vertex Array Object to save links between attributes and VBOs
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    // Initialize virtual buffer objects
+    glGenBuffers(1, &triangles_vbo_);
+    glGenBuffers(1, &lines_vbo_);
+
+    // Initialize element buffer objects
+    glGenBuffers(1, &triangles_ebo_);
+    glGenBuffers(1, &lines_ebo_);
+
+    // Initialize shaders
+    vertex_shader_ = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader_, 1, &kVertexShaderSource, NULL);
+    glCompileShader(vertex_shader_);
+
+    fragment_shader_ = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader_, 1, &kFragmentShaderSource, NULL);
+    glCompileShader(fragment_shader_);
+
+    // Create program
+    GLuint program = glCreateProgram();
+    glAttachShader(program, vertexShader);
+    glAttachShader(program, fragmentShader);
+    glLinkProgram(program);
+    glUseProgram(program);
+
+    GLint pos_attrib = glGetAttribLocation(program, "position");
+    glVertexAttribPointer(pos_attrib, 2, GL_FLOAT, GL_FALSE, 6*sizeof(float), 0);
+    glEnableVertexAttribArray(pos_attrib);
+
+    GLint color_attrib = glGetAttribLocation(program, "color");
+    glVertexAttribPointer(color_attrib, 4, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)2*sizeof(float));
+    glEnableVertexAttribArray(color_attrib);
+
+}
+
+bool SimpleGL::Update()
+{
+    SDL_Event window_event;
+    if (SDL_PollEvent(&window_event))
+    {
+        if (window_event.type == SDL_QUIT) {
+            SimpleGL::Destroy();
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void SimpleGL::Draw()
+{
+    // Update virtual buffer objects
+    glBindBuffer(GL_ARRAY_BUFFER, triangles_vbo_);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(triangle_vertices_.data()), triangle_vertices_.data(), GL_STATIC_DRAW);
+    triangle_vertices_.clear();
+
+    glBindBuffer(GL_ARRAY_BUFFER, lines_vbo_);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(line_vertices_.data()), line_vertices_.data(), GL_STATIC_DRAW);
+    line_vertices_.clear();
+
+    // Update element buffer objects
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, triangles_ebo_);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(triangle_elements_.data()), triangle_elements_.data(), GL_STATIC_DRAW);
+    triangle_elements_.clear();
+    
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, lines_ebo_);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(line_elements_.data()), line_elements_.data(), GL_STATIC_DRAW);
+    line_elements_.clear();
+}
+
+void SimpleGL::DrawTriangle(int x1, int y1, int x2, int y2, int x3, int y3, SimpleGL::Color color)
+{
+    
+}
+
+void SimpleGL::DrawRect(int x, int y, int width, int height, SimpleGL::Color color)
+{
+
+}
+
+void SimpleGL::DrawLine(int x1, int y1, int x2, int y2, SimpleGL::Color color)
 {
 }
 
-void SimpleGL::update()
+void SimpleGL::Destroy()
 {
-}
-
-void SimpleGL::drawRect(int x, int y, int width, int height)
-{
-}
-
-void SimpleGL::drawLine(int x1, int y1, int x2, int y2)
-{
+    SDL_GL_DeleteContext(gl_context_);
+    SDL_DestroyWindow(window_);
+    SDL_Quit();
 }
